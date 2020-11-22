5e17f4f1d32f05296ae575f798eb4e077810dc21
Mlumpkin14 <mattlumpkin04@gmail.com>
2020-11-22 11:09:58 -0500

    Added components and service layer

--- src/main/frontend/src/app/tasks/task.service.ts
+++ src/main/frontend/src/app/tasks/task.service.ts
@@ -1,17 +1,31 @@
 
-import {Injectable} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Task} from './task.model';
 
 
 
+
+
 @Injectable()
 export class TaskService {
+
+  onTaskAdded = new EventEmitter<Task[]>();
+
   constructor(private httpClient: HttpClient) {
 
   }
 
   getTasks(){
     return this.httpClient.get<Task[]>('/api/tasks');
+  }
+
+  saveTask(task: Task, checked: boolean){
+    task.completed = checked;
+    return this.httpClient.post<Task[]>('/api/tasks/save', task)
+  }
+
+  addTask(task: Task) {
+    return this.httpClient.post<Task[]>('/api/tasks/save', task)
   }
 }
